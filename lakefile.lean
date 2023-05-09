cfdeca46f7a5a2a21d5783e0b2082ba4fe1fd2f8
Joachim Breitner <mail@joachim-breitner.de>
2023-05-09 20:27:00 +0200

    Raw package

---
+++ lakefile.lean
@@ -0,0 +1,14 @@
+import Lake
+open Lake DSL
+
+package «rerolling-sixes» {
+  -- add package configuration options here
+}
+
+require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "master"
+
+
+lean_lib «RerollingSixes» {
+  -- add library configuration options here
+}
+
