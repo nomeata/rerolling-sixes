12191c4f8dd20759aac10c2ea4597ceebed06d83
Joachim Breitner <mail@joachim-breitner.de>
2023-05-14 20:41:55 +0200

    Move n=0 check to definition of v

--- RerollingSixes.lean
+++ RerollingSixes.lean
@@ -9,14 +9,14 @@ set_option autoImplicit false
 open BigOperators Nat
 
 /- Binomial Expectation operator -/
-def En (p : ℚ) (n : ℕ) (val : ∀ i, 0 < n -> i ≤ n -> ℚ) : ℚ :=
-  if hnz : n = 0 then 0
-  else ∑ j : Fin (n+1), 
+def En (p : ℚ) (n : ℕ) (val : ∀ i, i ≤ n -> ℚ) : ℚ :=
+∑ j : Fin (n+1), 
     p^(j : ℕ) * (1-p)^(n - j) * n.choose j *
-    val j (zero_lt_of_ne_zero hnz) (le_of_lt_succ j.2)
+    val j (le_of_lt_succ j.2)
 
 def v (p : ℚ) (n : ℕ) : ℚ :=
-  En p n (fun i _ hi =>
+  if hnz : n = 0 then 0
+  else En p n (fun i hi =>
     if hz : i = 0
     /- No heads -/
     then v p (n-1)
@@ -30,16 +30,17 @@ def v (p : ℚ) (n : ℕ) : ℚ :=
 decreasing_by decreasing_with aesop (add safe Nat.sub_lt, safe Nat.zero_lt_of_ne_zero)
 
 def vf (p : ℚ) (n : ℕ) (r : ∀ i, i < n -> ℚ) : ℚ :=
-  En p n (fun i hnz hi =>
+  if hnz : n = 0 then 0
+  else En p n (fun i hi =>
     if hz : i = 0
     /- No heads -/
-    then r (n-1) (Nat.sub_lt hnz zero_lt_one)
+    then r (n-1) (Nat.sub_lt (Nat.zero_lt_of_ne_zero hnz) zero_lt_one)
     /- i heads -/
     else 
       /- Fix 1 to i coins -/
       Finset.sup' (Finset.range i) 
       (by simp [hz])
-      (fun j => (j+1) + r (n-(j+1)) (Nat.sub_lt hnz (Nat.zero_lt_succ _)))
+      (fun j => (j+1) + r (n-(j+1)) (Nat.sub_lt (Nat.zero_lt_of_ne_zero hnz) (Nat.zero_lt_succ _)))
   )  
 
 -- Cannot use attr yet, due to extra parameters
@@ -57,88 +58,76 @@ lemma v_fast_v : v = fast_v := by
 -- set_option trace.Tactic.positivity true
 
 lemma En_nonneg {p} (hp : 0 ≤ p) (hp2 : p ≤ 1) (n)
-  (val : ∀ i, 0 < n -> i ≤ n -> ℚ)
-  (val_nonneg : ∀ i (hnz : 0 < n) (hin : i ≤ n), 0 ≤ val i hnz hin) :
+  (val : ∀ i, i ≤ n -> ℚ)
+  (val_nonneg : ∀ i (hin : i ≤ n), 0 ≤ val i hin) :
   0 ≤ En p n val := by
   have : 0 ≤ (1 - p) := by linarith -- to help the positivity tactic
   rw [En] 
-  split
-  case inl hnz => exact (le_refl _)
-  case inr hnz =>
-    apply Finset.sum_nonneg
-    intro i _
-    apply mul_nonneg; try positivity
-    apply val_nonneg
+  apply Finset.sum_nonneg
+  intro i _
+  apply mul_nonneg; try positivity
+  apply val_nonneg
 
 lemma v_nonneg {p} (hp : 0 ≤ p) (hp2 : p ≤ 1) n : 0 ≤ v p n := by
   induction' n using Nat.case_strong_induction_on
   case hz =>
     rewrite [v]
-    apply En_nonneg hp hp2
-    intro i hnz
-    cases hnz
+    simp
   case hi n IH =>
     have IHn := IH n (by linarith)
     rewrite [v]
+    simp
     apply En_nonneg hp hp2
-    intro i _hnz _hin
+    intro i _hin
     split
     case inl => exact IHn
     case inr hiz =>
       have hzero : 0 ∈ Finset.range i := by simp; apply zero_lt_of_ne_zero hiz
       apply Finset.le_sup'_of_le _ hzero
       positivity
-
 /--
 Show that En is normalized
 -/
-lemma En_const (p n) (hnz : 0 < n) (q : ℚ) :
-  En p n (fun _ _ _ => q) = q := by
+lemma En_const (p n) (q : ℚ) :
+  En p n (fun _ _ => q) = q := by
   rw [En]
-  split
-  case inl => aesop
-  case inr =>
-    rw [<- Finset.sum_mul]
-    rw [ mul_left_eq_self₀ ]
-    left
-    symm
-    calc
-    1 = (p + (1-p))^n := by simp
-    _ = Finset.sum (Finset.range (n + 1)) fun m =>
-      p ^ m * (1-p) ^ (n - m) * ↑(Nat.choose n m) := Commute.add_pow (by simp) _ 
-    _ = _ :=  by
-      rw [Finset.sum_fin_eq_sum_range]
-      apply Finset.sum_congr rfl
-      intro i hi
-      simp at hi
-      split
-      case inl => rfl
-      case inr h => contradiction
+  rw [<- Finset.sum_mul]
+  rw [ mul_left_eq_self₀ ]
+  left
+  symm
+  calc
+  1 = (p + (1-p))^n := by simp
+  _ = Finset.sum (Finset.range (n + 1)) fun m =>
+    p ^ m * (1-p) ^ (n - m) * ↑(Nat.choose n m) := Commute.add_pow (by simp) _ 
+  _ = _ :=  by
+    rw [Finset.sum_fin_eq_sum_range]
+    apply Finset.sum_congr rfl
+    intro i hi
+    simp at hi
+    split
+    case inl => rfl
+    case inr h => contradiction
 
 lemma En_mono {p} (hp : 0 ≤ p) (hp2 : p ≤ 1) (n)
-  (val1 : ∀ i, 0 < n -> i ≤ n -> ℚ)
-  (val2 : ∀ i, 0 < n -> i ≤ n -> ℚ)
-  (val_nonneg : ∀ i (hnz : 0 < n) (hin : i ≤ n), val1 i hnz hin <= val2 i hnz hin) :
+  (val1 : ∀ i, i ≤ n -> ℚ)
+  (val2 : ∀ i, i ≤ n -> ℚ)
+  (val_nonneg : ∀ i (hin : i ≤ n), val1 i hin <= val2 i hin) :
   En p n val1 ≤ En p n val2 := by
   have : 0 ≤ (1 - p) := by linarith
-  unfold En
-  split
-  case inl => rfl
-  case inr =>
-    apply Finset.sum_le_sum
-    intro i _
-    apply mul_le_mul_of_nonneg_left
-    apply val_nonneg
-    positivity
+  apply Finset.sum_le_sum
+  intro i _
+  apply mul_le_mul_of_nonneg_left
+  apply val_nonneg
+  positivity
 
 lemma v_mono_n {p} (hp : 0 ≤ p) (hp2 : p ≤ 1): Monotone (v p) := by
   have : 0 ≤ (1 - p) := by linarith
   apply monotone_nat_of_le_succ
   intro n
   conv => right ; rewrite [v] ; simp
-  conv => left; rw [<- En_const p (n+1) (by simp) (v p n)]
+  conv => left; rw [<- En_const p (n+1) (v p n)]
   apply En_mono hp hp2
-  intro i _hnz _hin
+  intro i _hin
   split
   case inl => simp
   case inr hiz =>
