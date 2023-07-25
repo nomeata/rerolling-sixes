2d1c5ba483017f65936e320e5de0fef3de947f3a
Joachim Breitner <mail@joachim-breitner.de>
2023-07-25 14:35:47 +0200

    Sort, better syntax, two maps

--- RerollingSixes/FindTheorems.lean
+++ RerollingSixes/FindTheorems.lean
@@ -12,6 +12,7 @@ open Mathlib.Tactic
 
 -- from Lean.Server.Completion
 -- from Mathlib.Tactic.Find
+-- (someone should put it somewhere public)
 private def isBlackListed (declName : Name) : MetaM Bool := do
   let env ← getEnv
   pure $ declName.isInternal
@@ -20,32 +21,49 @@ private def isBlackListed (declName : Name) : MetaM Bool := do
   <||> isRec declName
   <||> Meta.isMatcher declName
 
+private def NameRel := NameMap NameSet
 
+-- The declaration cache keeps library declarations separate
+-- from non-library declarations, cargo-culting a bit what's being done
+-- in Mathlib.Tactic.Cache.DiscrTreeCache
 
-initialize findDeclsByConsts : DeclCache (NameMap NameSet) ←
-  DeclCache.mk "#find_theorems: init cache" {} fun _ c m ↦ do
-    if (← isBlackListed c.name) then
-      return m
-    let consts := c.type.foldConsts {} (flip NameSet.insert)
-    return consts.fold (init := m) (fun m n =>
-      m.insert n (
-        m.findD n {} |> flip .insert c.name
-      )
+instance : EmptyCollection NameRel :=
+  inferInstanceAs $ EmptyCollection (NameMap NameSet)
+
+instance : Nonempty NameRel :=
+  inferInstanceAs $ Nonempty (NameMap NameSet)
+
+private def NameRel.addDecl (c : ConstantInfo) (m : NameRel) := do
+  if (← isBlackListed c.name) then
+    return m
+  let consts := c.type.foldConsts {} (flip NameSet.insert)
+  return consts.fold (init := m) fun m n =>
+    m.insert n (
+      m.findD n {} |> flip .insert c.name
     )
 
-syntax (name := find_theorems) "#find_theorems" "[" ident+ "]" : command
+initialize findDeclsByConsts : DeclCache (NameRel × NameRel) ←
+  DeclCache.mk 
+    (profilingName := "#find_theorems: init cache")
+    (empty := ({}, {}))
+    (addLibraryDecl := fun _ c (m₁, m₂) ↦ return (← NameRel.addDecl c m₁, m₂))
+    (addDecl := fun _ c (m₁, m₂) ↦ return (m₁, ← NameRel.addDecl c m₂))
 
 
--- Faster implementation possible?
+-- Faster implementations possible?
 def NameSet.intersects (ss : Array NameSet) : NameSet :=
   ss.back.fold (init := {}) fun s m =>
     if ss.pop.all (fun s' => s'.contains m) then s.insert m else s
+def NameSet.union (s₁ : NameSet) (s₂ : NameSet) : NameSet :=
+  s₂.fold (init := s₁) .insert
+
+syntax (name := find_theorems) withPosition("#find_theorems" (colGt ident)+) : command
 
 @[command_elab find_theorems]
 def findTheoremsElab : Elab.Command.CommandElab := λ stx => do
   profileitM Exception "find_theorems" (← getOptions) do
     match stx with
-    | `(#find_theorems [ $sidents:ident* ]) =>
+    | `(#find_theorems $sidents:ident*) =>
       let needles <- sidents.mapM fun sident => do
         let n := Lean.TSyntax.getId sident
         unless ((← getEnv).contains n) do
@@ -58,12 +76,14 @@ def findTheoremsElab : Elab.Command.CommandElab := λ stx => do
       --   if needles.all consts.elem
       --   then name :: es
       --   else es
-      let c <- Elab.Command.liftTermElabM findDeclsByConsts.get
-      let hits := NameSet.intersects (needles.map (fun needle => c.findD needle {}))
-      let hits_e <- hits.toList.mapM mkConstWithLevelParams
+      let (m₁, m₂) <- Elab.Command.liftTermElabM findDeclsByConsts.get
+      let hits := NameSet.intersects $ needles.map $ fun needle =>
+        NameSet.union (m₁.findD needle {}) (m₂.findD needle {})
+      let hits := hits.toArray.qsort Name.lt
+      let hits_e <- hits.mapM mkConstWithLevelParams
       Lean.logInfo $
-        m!"Found {hits_e.length} constants:" ++ Format.line ++
-        (MessageData.joinSep (hits_e.map ppConst) Format.line)
+        m!"Found {hits_e.size} constants:" ++ Format.line ++
+        (MessageData.joinSep (hits_e.toList.map ppConst) Format.line)
     | _ =>
       Elab.throwUnsupportedSyntax
 
